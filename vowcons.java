d7ea6977c57835efcf953079bf7ddb17a73476d2
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2018-09-25 11:53:34 +0530

    Create vowcons.java

---
+++ vowcons.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class JavaProgram
+{
+    public static void main(String args[])
+    {
+        char ch;
+        Scanner scan = new Scanner(System.in);
+		
+      
+        ch = scan.next().charAt(0);
+		
+        if((ch=='a' || ch=='e' ||
+        ch=='i'|| ch=='o' ||
+        ch=='u')&&(ch=='a' || ch=='E' ||
+        ch=='I'|| ch=='O' ||
+        ch=='U'))
+        {
+            System.out.print(" Vowel");
+        }
+        else
+        {
+            System.out.print("Consonant");
+        }
+    }
+}
